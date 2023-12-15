722508e23c86d3b5e1816074f0b9d164fbb93662
Carlos Ferreira <carlos@especializati.com.br>
2023-12-15 10:42:20 -0300

    chore: use composer with psr-4

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo 'test';
+use Core\Example;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$example = new Example;
+echo $example->index();
---
+++ src/Core/Example.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Core;
+
+class Example
+{
+    public function index(): string
+    {
+        return 'example';
+    }    
+}
