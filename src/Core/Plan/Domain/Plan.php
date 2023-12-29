7c3d42a8cc54ea48fe407803985aea4cbc4b4e3e
Carlos Ferreira <carlos@especializati.com.br>
2023-12-29 10:43:38 -0300

    refactor: use uuid value object

--- src/Core/Plan/Domain/Plan.php
+++ src/Core/Plan/Domain/Plan.php
@@ -2,13 +2,16 @@
 
 namespace Core\Plan\Domain;
 
+use Core\SeedWork\Domain\ValueObjects\Uuid;
+
 class Plan
 {
     public function __construct(
-        protected string $id,
+        protected ?Uuid $id = null,
         protected string $name,
         protected string $description,
     ) {
+        $this->id = $this->id ?? Uuid::random();
     }
     
     public function __get($property)
