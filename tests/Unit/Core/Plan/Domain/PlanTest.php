20776640f9423c4463745a0a4c08f32acd6273ce
Carlos Ferreira <carlos@especializati.com.br>
2023-12-29 10:43:52 -0300

    test: use uuid value object

--- tests/Unit/Core/Plan/Domain/PlanTest.php
+++ tests/Unit/Core/Plan/Domain/PlanTest.php
@@ -1,19 +1,20 @@
 <?php
 
 use Core\Plan\Domain\Plan;
+use Core\SeedWork\Domain\ValueObjects\Uuid;
 
 it('should access all properties', function () {
+    $id = Uuid::random();
     $plan = new Plan(
-        id: 'uuid-value',
+        id: $id,
         name: 'plan name',
         description: 'desc plan'
     );
-    expect($plan->id)->toBe('uuid-value');
-    expect($plan->id())->toBe('uuid-value');
+    expect($plan->id)->toBe($id);
+    expect($plan->id())->toBe((string) $id);
     expect($plan->name)->toBe('plan name');
     expect($plan->description)->toBe('desc plan');
 
-    expect($plan->id)->toBeString();
     expect($plan->id())->toBeString();
     expect($plan->name)->toBeString();
     expect($plan->description)->toBeString();
