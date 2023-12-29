4498a9c2b1a16970135f649e459710ace43f3a83
Carlos Ferreira <carlos@especializati.com.br>
2023-12-29 10:09:59 -0300

    feat: define properties entity

--- src/Core/Plan/Domain/Plan.php
+++ src/Core/Plan/Domain/Plan.php
@@ -4,5 +4,20 @@ namespace Core\Plan\Domain;
 
 class Plan
 {
+    public function __construct(
+        protected string $id,
+        protected string $name,
+        protected string $description,
+    ) {
+    }
     
+    public function __get($property)
+    {
+        return $this->{$property};
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
 }
