9cf675fac7a86b39d6591059116f53574cb91397
Carlos Ferreira <carlos@especializati.com.br>
2023-12-29 10:48:56 -0300

    test: should use uuid passed

--- tests/Unit/Core/Plan/Domain/PlanTest.php
+++ tests/Unit/Core/Plan/Domain/PlanTest.php
@@ -4,6 +4,20 @@ use Core\Plan\Domain\Plan;
 use Core\SeedWork\Domain\ValueObjects\Uuid;
 
 it('should access all properties', function () {
+    $plan = new Plan(
+        name: 'plan name',
+        description: 'desc plan'
+    );
+    expect($plan->name)->toBe('plan name');
+    expect($plan->description)->toBe('desc plan');
+
+    expect($plan->id)->not->toBeNull();
+    expect($plan->id())->toBeString();
+    expect($plan->name)->toBeString();
+    expect($plan->description)->toBeString();
+});
+
+it('should use uuid passed', function () {
     $id = Uuid::random();
     $plan = new Plan(
         id: $id,
@@ -12,10 +26,4 @@ it('should access all properties', function () {
     );
     expect($plan->id)->toBe($id);
     expect($plan->id())->toBe((string) $id);
-    expect($plan->name)->toBe('plan name');
-    expect($plan->description)->toBe('desc plan');
-
-    expect($plan->id())->toBeString();
-    expect($plan->name)->toBeString();
-    expect($plan->description)->toBeString();
 });
