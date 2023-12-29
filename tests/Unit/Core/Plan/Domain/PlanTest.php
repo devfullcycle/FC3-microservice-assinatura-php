2303b11fc7d7f1c81c586f6933e595454758adc0
Carlos Ferreira <carlos@especializati.com.br>
2023-12-29 10:10:12 -0300

    test: access properties entity

--- tests/Unit/Core/Plan/Domain/PlanTest.php
+++ tests/Unit/Core/Plan/Domain/PlanTest.php
@@ -2,7 +2,19 @@
 
 use Core\Plan\Domain\Plan;
 
-it('should exists entity', function () {
-    $plan = new Plan;
-    expect(true)->toBe(true);
+it('should access all properties', function () {
+    $plan = new Plan(
+        id: 'uuid-value',
+        name: 'plan name',
+        description: 'desc plan'
+    );
+    expect($plan->id)->toBe('uuid-value');
+    expect($plan->id())->toBe('uuid-value');
+    expect($plan->name)->toBe('plan name');
+    expect($plan->description)->toBe('desc plan');
+
+    expect($plan->id)->toBeString();
+    expect($plan->id())->toBeString();
+    expect($plan->name)->toBeString();
+    expect($plan->description)->toBeString();
 });
