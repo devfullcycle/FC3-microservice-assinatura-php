4171c3bd61d5a1ed388126d1a7ca94e1e7d01374
Carlos Ferreira <carlos@especializati.com.br>
2024-01-13 11:16:40 -0300

    feat: validation str max length

--- tests/Unit/Core/SeedWork/Validators/DomainValidatorTest.php
+++ tests/Unit/Core/SeedWork/Validators/DomainValidatorTest.php
@@ -2,6 +2,7 @@
 
 use Core\SeedWork\Domain\Exceptions\EntityValidationException;
 use Core\SeedWork\Domain\Validators\DomainValidator;
+use Faker\Factory;
 
 test('should throw exception when value is null', function () {
     DomainValidator::notNull(value: null);
@@ -10,3 +11,8 @@ test('should throw exception when value is null', function () {
 test('should throw exception when value is null - and custom message exception', function () {
     DomainValidator::notNull(value: null, exceptionMessage: 'not be null');
 })->throws(EntityValidationException::class, 'not be null');
+
+test('should be string and max characters', function () {
+    $value = Factory::create()->sentence(255);
+    DomainValidator::strMaxLength(value: $value);
+})->throws(EntityValidationException::class, 'The value must not be greater than 255 characters');
