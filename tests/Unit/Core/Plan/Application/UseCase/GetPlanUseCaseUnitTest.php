3fd973a8083818652012acac446b042de8381aa4
Carlos Ferreira <carlos@especializati.com.br>
2024-01-27 09:33:06 -0300

    test: refactor - create mock of repository

--- tests/Unit/Core/Plan/Application/UseCase/GetPlanUseCaseUnitTest.php
+++ tests/Unit/Core/Plan/Application/UseCase/GetPlanUseCaseUnitTest.php
@@ -8,45 +8,17 @@ use Core\Plan\Domain\Repositories\PaginationInterface;
 use Core\Plan\Domain\Repositories\PlanRepositoryInterface;
 
 test('should return plan', function () {
-    class anonymous implements PlanRepositoryInterface {
-        public function insert(Plan $plan): Plan
-        {
-            throw new \Exception('Not implemented');
-        }
-
-        public function findById(string $id): Plan
-        {
-            return new Plan('name_plan', 'description_plan');
-        }
-
-        /**
-         * @return stdClass[]
-         */
-        public function findAll(string $filter = '', string $orderBy = 'DESC'): array
-        {
-            throw new \Exception('Not implemented');
-        }
-
-        public function paginate(string $filter = '', string $orderBy = 'DESC', int $page = 1, int $totalPerPage = 15): PaginationInterface
-        {
-            throw new \Exception('Not implemented');
-        }
-
-        public function update(Plan $plan): Plan
-        {
-            throw new \Exception('Not implemented');
-        }
-
-        public function delete(string $id): bool
-        {
-            throw new \Exception('Not implemented');
-        }
-    }
-    $useCase = new GetPlanUseCase(
-        repository: new anonymous()
-    );
     $dto = new InputPlanDTO(
         id: '123.123.3212'
+    );
+
+    $mockRepository = Mockery::mock(PlanRepositoryInterface::class);
+    $mockRepository->shouldReceive('findById')
+                    ->times(1)
+                    ->with($dto->id)
+                    ->andReturn(new Plan('name_plan', 'description_plan'));
+    $useCase = new GetPlanUseCase(
+        repository: $mockRepository
     );
     $response = $useCase->execute(input: $dto);
 
