79ea38c89b7a50a14921393c25207694d54d8e7e
Carlos Ferreira <carlos@especializati.com.br>
2024-02-02 10:54:43 -0300

    refactor: return in method exec output dto

--- src/Core/Plan/Application/UseCase/GetPlansUseCase.php
+++ src/Core/Plan/Application/UseCase/GetPlansUseCase.php
@@ -2,6 +2,7 @@
 
 namespace Core\Plan\Application\UseCase;
 
+use Core\Plan\Application\DTO\OutputPlansDTO;
 use Core\Plan\Domain\Repositories\PlanRepositoryInterface;
 
 class GetPlansUseCase
@@ -10,8 +11,18 @@ class GetPlansUseCase
     {
     }
 
-    public function execute(): void
+    public function execute(): OutputPlansDTO
     {
-        
+        $response = $this->repository->paginate();
+
+        return new OutputPlansDTO(
+            items: $response->items(),
+            total: $response->total(),
+            lastPage: $response->lastPage(),
+            firstPage: $response->firstPage(),
+            totalPerPage: $response->totalPerPage(),
+            nextPage: $response->nextPage(),
+            previousPage: $response->previousPage(),
+        );
     }
 }
