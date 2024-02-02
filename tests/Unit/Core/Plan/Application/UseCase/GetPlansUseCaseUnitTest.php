8aeea1af7c0e216aa65db181c77f677d36ee043a
Carlos Ferreira <carlos@especializati.com.br>
2024-02-02 11:05:08 -0300

    test: should return dto with pagination stub

--- tests/Unit/Core/Plan/Application/UseCase/GetPlansUseCaseUnitTest.php
+++ tests/Unit/Core/Plan/Application/UseCase/GetPlansUseCaseUnitTest.php
@@ -1,14 +1,20 @@
 <?php
 
+use Core\Plan\Application\DTO\OutputPlansDTO;
 use Core\Plan\Application\UseCase\GetPlansUseCase;
 use Core\Plan\Domain\Repositories\PlanRepositoryInterface;
+use Tests\Stubs\PaginationStub;
 
 test('should get all plans', function () {
+    $stubPagination = new PaginationStub();
     $mockRepository = Mockery::mock(PlanRepositoryInterface::class);
+    $mockRepository->shouldReceive('paginate')
+                ->times(1)
+                ->andReturn($stubPagination);
     $useCase = new GetPlansUseCase(
         repository: $mockRepository
     );
 
-    $useCase->execute();
-    expect(true)->toBe(true);
+    $response = $useCase->execute();
+    expect($response)->toBeInstanceOf(OutputPlansDTO::class);
 });
