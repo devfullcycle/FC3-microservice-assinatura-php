1175da2571917d2116ae22dd6ce06e39affc5f7a
Carlos Ferreira <carlos@especializati.com.br>
2024-03-02 10:47:13 -0300

    feat: should update entity plan

---
+++ src/Core/Plan/Application/UseCase/EditPlanUseCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\Plan\Application\UseCase;
+
+use Core\Plan\Application\DTO\EditPlanDTO;
+use Core\Plan\Application\DTO\OutputPlanDTO;
+use Core\Plan\Domain\Entities\Plan;
+use Core\Plan\Domain\Repositories\PlanRepositoryInterface;
+
+class EditPlanUseCase
+{
+    public function __construct(
+        private PlanRepositoryInterface $repository
+    ) {}
+
+    public function execute(EditPlanDTO $input): OutputPlanDTO
+    {
+        $plan = $this->repository->findById($input->id);
+        $plan->update(
+            name: $input->name,
+            description: $input->description
+        );
+        $entityUpdated = $this->repository->update($plan);
+
+        return OutputPlanDTO::fromEntity($entityUpdated);
+    }
+}
