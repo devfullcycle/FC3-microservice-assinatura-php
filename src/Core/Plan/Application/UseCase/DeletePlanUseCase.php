5e1fff3c1010c48a92f45f41ad123a9d69a78600
Carlos Ferreira <carlos@especializati.com.br>
2024-03-02 10:59:31 -0300

    feat: should delete a plan

---
+++ src/Core/Plan/Application/UseCase/DeletePlanUseCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\Plan\Application\UseCase;
+
+use Core\Plan\Application\DTO\DeleteOutputPlanDTO;
+use Core\Plan\Application\DTO\InputPlanDTO;
+use Core\Plan\Application\DTO\InputPlansDTO;
+use Core\Plan\Application\DTO\OutputPlanDTO;
+use Core\Plan\Application\DTO\OutputPlansDTO;
+use Core\Plan\Domain\Repositories\PlanRepositoryInterface;
+
+class DeletePlanUseCase
+{
+    public function __construct(private PlanRepositoryInterface $repository)
+    {
+    }
+
+    public function execute(InputPlanDTO $input): DeleteOutputPlanDTO
+    {
+        $entity = $this->repository->findById($input->id);
+        $response = $this->repository->delete($entity->id());
+
+        return new DeleteOutputPlanDTO(
+            deleted: $response
+        );
+    }
+}
