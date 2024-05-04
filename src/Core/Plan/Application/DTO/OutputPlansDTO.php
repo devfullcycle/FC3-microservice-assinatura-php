674112acdd2918fdd9d869e7c58b7f2d49d25c59
Carlos Ferreira (perfil 2) <contato@especializati.com.br>
2024-05-04 10:41:18 -0300

    fix: properties can be null

--- src/Core/Plan/Application/DTO/OutputPlansDTO.php
+++ src/Core/Plan/Application/DTO/OutputPlansDTO.php
@@ -11,10 +11,10 @@ class OutputPlansDTO
         public array $items,
         public int $total,
         public int $last_page,
-        public int $first_page,
         public int $total_per_page,
-        public int $next_page,
-        public int $previous_page,
+        public ?int $first_page,
+        public ?int $next_page,
+        public ?int $previous_page,
     ) {
     }
 }
