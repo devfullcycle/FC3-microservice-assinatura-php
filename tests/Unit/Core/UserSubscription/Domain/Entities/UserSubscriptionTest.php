60b0c62d40c60e672c648e2ca9318519d4ed1b2c
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-05-18 11:03:27 -0300

    test: should use uuid passed

--- tests/Unit/Core/UserSubscription/Domain/Entities/UserSubscriptionTest.php
+++ tests/Unit/Core/UserSubscription/Domain/Entities/UserSubscriptionTest.php
@@ -3,6 +3,7 @@
 use Core\Plan\Domain\Entities\Plan;
 use Core\SeedWork\Domain\ValueObjects\Address;
 use Core\SeedWork\Domain\ValueObjects\CpfVO;
+use Core\SeedWork\Domain\ValueObjects\Uuid;
 use Core\User\Domain\Entities\User;
 use Core\UserSubscription\Domain\Entities\UserSubscription;
 
@@ -16,11 +17,11 @@ beforeEach(function () {
         zipCode: '75700-000',
         number: 1212,
     );
-    $user = new User(name: 'John', lastName: 'Doe', age: 31, address: $address, type: $cpfVo);
-    $plan = new Plan(name: 'Basic', description: 'Basic plan');
+    $this->user = new User(name: 'John', lastName: 'Doe', age: 31, address: $address, type: $cpfVo);
+    $this->plan = new Plan(name: 'Basic', description: 'Basic plan');
     $this->subscription = new UserSubscription(
-        user: $user,
-        plan: $plan,
+        user: $this->user,
+        plan: $this->plan,
         endsAt: new DateTime('12-12-2000'),
         lastBilling: new DateTime('12-12-2000'),
         active: true,
@@ -35,4 +36,19 @@ test('should valid type properties', function () {
     expect($this->subscription->lastBilling)->toBeInstanceOf(Datetime::class);
     expect($this->subscription->active)->toBeBool();
     expect($this->subscription->cancelled)->toBeBool();
-})->only();
+});
+
+test('should use uuid passed', function () {
+    $id = Uuid::random();
+    $subscription = new UserSubscription(
+        id: $id,
+        user: $this->user,
+        plan: $this->plan,
+        endsAt: new DateTime('12-12-2000'),
+        lastBilling: new DateTime('12-12-2000'),
+        active: true,
+        cancelled: false,
+    );
+    expect($subscription->id)->toBe($id);
+    expect($subscription->id())->toBe((string) $id);
+});
