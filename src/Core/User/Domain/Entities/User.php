abebb29bde1650604d3e606c591ea4f92978863a
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-05-18 08:30:56 -0300

    feat: define entity of user

---
+++ src/Core/User/Domain/Entities/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core\User\Domain\Entities;
+
+use Core\SeedWork\Domain\Traits\MethodsMagicTrait;
+use Core\SeedWork\Domain\Validators\DomainValidator;
+use Core\SeedWork\Domain\ValueObjects\Uuid;
+
+class User
+{
+    use MethodsMagicTrait;
+
+    public function __construct(
+        protected string $name,
+        protected string $lastName,
+        protected int $age,
+        protected ?Uuid $id = null,
+    ) {
+        $this->id = $this->id ?? Uuid::random();
+        $this->validate();
+    }
+
+    public function update(string $name, string $lastName, int $age = null): void
+    {
+        $this->name = $name;
+        $this->lastName = $lastName;
+        $this->age = $age ?? $this->age;
+
+        $this->validate();
+    }
+
+    private function validate(): void
+    {
+        DomainValidator::strMinLength($this->name);
+        DomainValidator::strMinLength($this->lastName);
+        DomainValidator::strMaxLength($this->name);
+        DomainValidator::strMaxLength($this->lastName);
+        DomainValidator::ageIsValid($this->age);
+    }
+}
