3d625240a04515012281f0e5a6c0f1b2babe7ad8
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-05-18 10:20:16 -0300

    feat: validate entity - property price

--- src/Core/PlanCost/Domain/Entities/PlanCost.php
+++ src/Core/PlanCost/Domain/Entities/PlanCost.php
@@ -3,6 +3,7 @@
 namespace Core\PlanCost\Domain\Entities;
 
 use Core\PlanCost\Domain\Enums\RecurrencePeriodEnum;
+use Core\SeedWork\Domain\Exceptions\EntityValidationException;
 use Core\SeedWork\Domain\Traits\MethodsMagicTrait;
 use Core\SeedWork\Domain\ValueObjects\Uuid;
 
@@ -27,6 +28,9 @@ class PlanCost
 
     private function validate(): void
     {
-        //
+        $expression = '/^\d{1,3}(\.\d{3})*(,\d{2})?$|^\d{1,3}(,\d{3})*(\.\d{2})?$|^\d+(\.\d{2})?$/';
+        if (! preg_match($expression, $this->price)) {
+            throw new EntityValidationException('Invalid price');
+        }
     }
 }
