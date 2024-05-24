eabe7bdad885b8f4ab1a92eeac5d5eac098907c6
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-05-24 11:41:14 -0300

    fix: set column id equals uuid

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('last_name');
             $table->integer('age');
