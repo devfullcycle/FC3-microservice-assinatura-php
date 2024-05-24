608fbf272e2aa9f1717367dd56412171a000bdfb
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-05-24 10:49:00 -0300

    feat: create user repository interface

---
+++ src/Core/User/Domain/Repositories/UserRepositoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Core\User\Domain\Repositories;
+
+use Core\User\Domain\Entities\User;
+use Core\SeedWork\Domain\Repositories\PaginationInterface;
+
+interface UserRepositoryInterface
+{
+    public function insert(User $plan): User;
+
+    public function findById(string $id): User;
+
+    /**
+     * @return User[]
+     */
+    public function findAll(string $filter = '', string $orderBy = 'DESC'): array;
+
+    public function paginate(string $filter = '', string $orderBy = 'DESC', int $page = 1, int $totalPerPage = 15): PaginationInterface;
+
+    public function update(User $plan): ?User;
+
+    public function delete(string $id): bool;
+}
