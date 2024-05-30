86a51058d0f7a826e7e80d965bd84ae0b03f40fd
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-05-30 21:05:02 +0000

    feat: define model and migration user subscription

---
+++ app/Models/UserSubscription.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserSubscription extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'plan_id',
+        'ends_at',
+        'last_billing',
+        'active',
+        'canceled',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
+}
---
+++ database/migrations/2024_05_30_171528_create_user_subscriptions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_subscriptions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->references('id')->on('users');
+            $table->foreignUuid('plan_id')->references('id')->on('plans');
+            $table->date('ends_at');
+            $table->date('last_billing');
+            $table->boolean('active')->default(true);
+            $table->boolean('canceled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_subscriptions');
+    }
+};
