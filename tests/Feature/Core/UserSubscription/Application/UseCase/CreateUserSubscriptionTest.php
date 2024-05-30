c9c7565b39c5ac279b007e0df4fa1a8fd7943ff7
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-05-30 21:06:04 +0000

    test: should create new user subscription

---
+++ tests/Feature/Core/UserSubscription/Application/UseCase/CreateUserSubscriptionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Address as AddressModel;
+use App\Models\User as UserModel;
+use App\Models\Plan as PlanModel;
+use Core\UserSubscription\Application\DTO\CreateUserSubscriptionDTO;
+use Core\UserSubscription\Application\DTO\OutputUserSubscription;
+use Core\UserSubscription\Application\UseCase\CreateUserSubscription;
+
+use function Pest\Laravel\assertDatabaseHas;
+
+test('should create new user subscription', function () {
+    $useCase = app(CreateUserSubscription::class);
+    $user = UserModel::factory()->create(['type' => 'cpf']);
+    AddressModel::factory()->create(['user_id' => $user->id]);
+    $plan = PlanModel::factory()->create();
+
+    $response = $useCase->execute(new CreateUserSubscriptionDTO(
+        userId: $user->id,
+        planId: $plan->id,
+        endsAt: '2029-01-01',
+        lastBilling: '2029-01-01',
+        active: true,
+        cancelled: false
+    ));
+
+    expect($response)->toBeInstanceOf(OutputUserSubscription::class);
+    expect($response->user->name)->toBe($user->name);
+    expect($response->plan->name)->toBe($plan->name);
+    expect($response->last_billing)->toBe('2029-01-01 00:00:00');
+    expect($response->ends_at)->toBe('2029-01-01 00:00:00');
+    expect($response->id)->not->toBeNull();
+    assertDatabaseHas('user_subscriptions', [
+        'user_id' => $user->id,
+        'plan_id' => $plan->id
+    ]);
+});
