9e94a4402959ec34ebe5b1b011e0fbe9af88702c
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-06-10 10:20:05 +0000

    test: get plans with pagination

--- tests/Feature/Api/PlanApiTest.php
+++ tests/Feature/Api/PlanApiTest.php
@@ -19,11 +19,11 @@ test('should get all plans - with empty plans', function () {
         ]);
 });
 
-test('should get all plans', function () {
-    Plan::factory(10)->create();
+test('should get paginate plans', function () {
+    Plan::factory(20)->create();
     getJson(route('plans.index'))
         ->assertStatus(200)
-        ->assertJsonCount(10, 'data')
+        ->assertJsonCount(15, 'data')
         ->assertJsonStructure([
             'data' => [
                 '*' => [
@@ -40,4 +40,42 @@ test('should get all plans', function () {
                 'previous_page',
             ],
         ]);
+});
+
+test('should get paginate plans - page 2', function () {
+    Plan::factory(20)->create();
+    getJson(route('plans.index') . '?page=2')
+        ->assertStatus(200)
+        ->assertJsonCount(5, 'data')
+        ->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'description',
+                ],
+            ],
+            'meta' => [
+                'total',
+                'last_page',
+                'first_page',
+                'next_page',
+                'previous_page',
+            ],
+        ]);
+});
+
+test('should get paginate plans - total per page', function () {
+    Plan::factory(20)->create();
+    getJson(route('plans.index') . '?per_page=20')
+        ->assertOk()
+        ->assertJsonCount(20, 'data');
+});
+
+test('should get paginate plans - with filter', function () {
+    Plan::factory(10)->create();
+    Plan::factory(10)->create(['name' => 'custom']);
+    getJson(route('plans.index') . '?filter=custom')
+        ->assertOk()
+        ->assertJsonCount(10, 'data');
 });
