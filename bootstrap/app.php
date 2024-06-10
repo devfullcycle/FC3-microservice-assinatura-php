a14b688f6fab601731c8c431782678ecdbe977c6
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-06-10 10:44:36 +0000

    refactor: intercept exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Core\SeedWork\Domain\Exceptions\EntityNotFoundException;
+use Core\SeedWork\Domain\Exceptions\EntityValidationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,5 +17,15 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (EntityNotFoundException $exception) {
+            return response()->json([
+                'message' => $exception->getMessage(),
+            ], 404);
+        });
+
+        $exceptions->render(function (EntityValidationException $exception) {
+            return response()->json([
+                'message' => $exception->getMessage(),
+            ], 422);
+        });
     })->create();
